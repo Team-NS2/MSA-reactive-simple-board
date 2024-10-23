1e2ee0cae349bdb8ab4a59d7994a469922d5bdc4
devhoi <tmddus828@gmail.com>
2024-10-23 12:56:18 +0900

    [init] discovery / gateway server

---
+++ discovery/src/test/java/com/ns2/app/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns2.app.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
